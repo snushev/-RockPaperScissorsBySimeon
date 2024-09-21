3fcf4b62e8e5615545b5f5abef9b5ec8a83349ec
Simeon Nushev <s.nushev@gmail.com>
2024-09-21 19:33:03 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,34 +13,49 @@ rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 
-player_move = input(f"{GREEN}Choose {BLUE}[r]{RESET}ock, {BLUE}[p]{RESET}aper, {BLUE}[s]{RESET}cissors: ")
+player_score = 0
+computer_score = 0
 
-if player_move == "r":
-    player_choice = rock
-elif player_move == "p":
-    player_choice = paper
-elif player_move == "s":
-    player_choice = scissors
+print(f"{ORANGE}Welcome to the Rock-Paper-Scissors Game!{RESET}")
 
-else:
-    raise SystemExit("Invalid Input. Try again...")
-print(f"You chose {YELLOW}{player_choice}{RESET}")
+while True:
+    player_move = input(f"{GREEN}Choose {BLUE}[r]{RESET}ock, {BLUE}[p]{RESET}aper, {BLUE}[s]{RESET}cissors: ")
 
-computer_choice = randint(1, 3)
+    if player_move == "r":
+        player_choice = rock
+    elif player_move == "p":
+        player_choice = paper
+    elif player_move == "s":
+        player_choice = scissors
 
-if computer_choice == 1:
-    computer_choice = rock
-elif computer_choice == 2:
-    computer_choice = paper
-else:
-    computer_choice = scissors
-print(f'The computer chose {YELLOW}{computer_choice}{RESET}')
+    else:
+        raise SystemExit("Invalid Input. Try again...")
+    print(f"You chose {YELLOW}{player_choice}{RESET}")
 
-if (player_choice == rock and computer_choice == scissors) or \
-        (player_choice == scissors and computer_choice == paper) or \
-        (player_choice == paper and computer_choice == rock):
-    print(f"{GREEN}You win!{RESET}")
-elif player_choice == computer_choice:
-    print(f"{GREEN}Draw!{RESET}")
-else:
-    print(f"{GREEN}You lose!")
+    computer_choice = randint(1, 3)
+
+    if computer_choice == 1:
+        computer_choice = rock
+    elif computer_choice == 2:
+        computer_choice = paper
+    else:
+        computer_choice = scissors
+    print(f'The computer chose {YELLOW}{computer_choice}{RESET}')
+
+    if (player_choice == rock and computer_choice == scissors) or \
+            (player_choice == scissors and computer_choice == paper) or \
+            (player_choice == paper and computer_choice == rock):
+        print(f"{GREEN}You win!{RESET}")
+        player_score += 1
+    elif player_choice == computer_choice:
+        print(f"{GREEN}Draw!{RESET}")
+    else:
+        print(f"{GREEN}You lose!")
+        computer_score += 1
+
+    print(f'{YELLOW}Your score: {RESET}{BLUE}{player_score}{RESET}')
+    print(f'{YELLOW}Computer score: {RESET}{BLUE}{computer_score}{RESET}')
+    play_again = input(f"{GREEN}Do you want to play again? {BLUE}[y]{RESET}/{BLUE}[n]{RESET}: ")
+    if play_again.lower() != "y":
+        print(f"{GREEN}Thank you for playing! {RESET}")
+        break
